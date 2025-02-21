cead13f4063d8b0893a9a2806cf4d95c757e8be6
Policken66 <komlev.artem.02@mail.ru>
2025-02-22 01:19:24 +0700

    Добавлена начальная структура проекта. Ui-файл с базовым интерфейсом приложения. Views/view_foothold_city -- для настройки именно визуального отображения, настройка виджетов(например покраска кнопок и задание им стиля) Controllers/controller_foothold_city -- для настройки логики работы интерфейса, подключения кнопок к функциям и подобное

---
+++ Foothold_city/Controllers/controller_foothold_city.py
@@ -0,0 +1,20 @@
+class ControllerFootholdCity:
+    """Контроллер для обработки событий"""
+
+    def __init__(self, view):
+        """
+        Инициализация контроллера.
+        :param view: Экземпляр класса представления (View).
+        """
+        self.view = view  # Сохраняем ссылку на представление
+
+        # Подключение сигналов кнопок к соответствующим обработчикам событий
+        self.view.ui.pushButton_open.clicked.connect(self.pushButton_open_clicked)
+        self.view.ui.pushButton_save.clicked.connect(self.pushButton_save_clicked)
+
+    def pushButton_open_clicked(self):
+        print("Button open clicked")
+
+    def pushButton_save_clicked(self):
+        print("Button save clicked")
+
